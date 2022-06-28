ffe4454240a819241a6383ee49e683702ff0dc5a
Elon Salfati <elon@salfati.io>
2022-06-28 16:18:23 +0300

    added chariot to the customer\'s list

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <header class="relative w-screen h-[calc(100vh-10em)] flex items-center justify-center">
+    <header class="relative w-screen h-[calc(100vh-8em)] flex items-center justify-center">
       <div class="relative max-w-5xl mx-auto">
         <div class="flex items-end justify-center bg-gradient-to-br from-sky-600 to-sky-400 bg-clip-text text-transparent text-center">
           <div>
@@ -17,13 +17,18 @@
         <p class="mt-6 text-lg text-slate-600 text-center max-w-3xl mx-auto">
           Focusing on the true innovation and bold visions of the future.
         </p>
-
-        <!-- <div class="mt-6 sm:mt-10 flex justify-center space-x-6 text-sm">
-          <NuxtLink class="text-sky-600 hover:text-sky-500 font-semibold text-lg" to="/companies">
-            Companies
-          </NuxtLink>
-        </div> -->
       </div>
     </header>
+
+    <div class="px-8 flex flex-col items-center justify-center">
+      <h2 class="mb-3">
+        Creating a beacon of innovation together.
+      </h2>
+      <div class="flex items-center justify-center">
+        <nuxt-link to="https://givechariot.com">
+          <img src="~~/assets/images/customers/chariot.png" alt="Chariot">
+        </nuxt-link>
+      </div>
+    </div>
   </div>
 </template>
